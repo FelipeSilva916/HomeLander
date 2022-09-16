9590c944e3264898f1adc24ecea4874c4f9e72b9
Felipe Silva <felipesilva916@gmail.com>
2022-09-16 13:51:59 -0700

    seeders

--- backend/db/seeders/20220916192349-Favorites.js
+++ backend/db/seeders/20220916192349-Favorites.js
@@ -1,24 +1,32 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("Favorites", [
+      {
+        id: 1,
+        userId: 1,
+        campsiteId: 1
+      },
+      {
+        id: 2,
+        userId: 1,
+        campsiteId: 2
+      },
+      {
+        id: 3,
+        userId: 2,
+        campsiteId: 3
+      },
+      {
+        id: 4,
+        userId: 2,
+        campsiteId: 2
+      }
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Favorites", null, {});
   }
 };
--- backend/db/seeders/20220916192355-Reviews.js
+++ backend/db/seeders/20220916192355-Reviews.js
@@ -1,24 +1,40 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("Reviews", [
+      {
+        id: 1,
+        userId: 1,
+        campsiteId: 1,
+        body: "This place is awesome!",
+        rating: 5
+      },
+      {
+        id: 2,
+        userId: 1,
+        campsiteId: 2,
+        body: "Nothing like this hidden gem!",
+        rating: 5
+      },
+      {
+        id: 3,
+        userId: 2,
+        campsiteId: 3,
+        body: "This place is amazing, worth the trek!",
+        rating: 5
+      },
+      {
+        id: 4,
+        userId: 2,
+        campsiteId: 2,
+        body: "This place is amazing, but hard to get to!",
+        rating: 5
+      }
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Reviews", null, {});
   }
 };
