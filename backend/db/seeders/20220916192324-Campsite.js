e3dda8bda430234712f51984cb983d98f2f5123f
Felipe Silva <felipesilva916@gmail.com>
2022-09-16 13:32:32 -0700

    Update 20220916192324-Campsite.js

--- backend/db/seeders/20220916192324-Campsite.js
+++ backend/db/seeders/20220916192324-Campsite.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -10,10 +10,47 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
+    await queryInterface.bulkInsert("Campsites", [
+      {
+        id: 1,
+        userId: 1,
+        latitude: 37.4003,
+        longitude: -118.7531,
+        name: "Little Lakes Valley",
+        previewImage:
+          "https://homelander.s3.us-west-1.amazonaws.com/littleLakes3.jpg"
+      },
+      {
+        id: 2,
+        userId: 1,
+        latitude: 37.1665,
+        longitude: -118.6356,
+        name: "Moonlight Falls",
+        previewImage:
+          "https://homelander.s3.us-west-1.amazonaws.com/moonLitFalls1.jpg"
+      },
+      {
+        id: 3,
+        userId: 1,
+        latitude: -50.9353,
+        longitude: -72.9079,
+        name: "Torres De Paine",
+        previewImage:
+          "https://homelander.s3.us-west-1.amazonaws.com/patagonia1.jpg"
+      },
+      {
+        id: 4,
+        userId: 2,
+        latitude: 43.70916,
+        longitude: 110.5893,
+        name: "Grand Teton National Park",
+        previewImage: "https://homelander.s3.us-west-1.amazonaws.com/teton1.jpg"
+      }
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
