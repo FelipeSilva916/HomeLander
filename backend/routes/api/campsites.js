d0d713e19da9a543bb69095c5418fd7c6b942356
Felipe Silva <felipesilva916@gmail.com>
2022-09-17 10:13:55 -0700

    Edit campsites

--- backend/routes/api/campsites.js
+++ backend/routes/api/campsites.js
@@ -18,6 +18,7 @@ router.post("/", requireAuth, validateCampsite, async (req, res) => {
     description
   });
   res.json(newCampsite);
+  res.status(201);
 });
 
 //========= GET /api/campsites - Get all campsites =========//
@@ -30,6 +31,27 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   const campsite = await Campsite.findByPk(req.params.id);
   res.json(campsite);
+});
+
+//========= PUT /api/campsites/:id - Update a single campsite =========//
+router.put("/:id", requireAuth, validateCampsite, async (req, res) => {
+  const { user } = req;
+  const { latitude, longitude, name, previewImage, description } = req.body;
+  const campsite = await Campsite.findByPk(req.params.id);
+  if (campsite.userId === user.id) {
+    await campsite.update({
+      latitude,
+      longitude,
+      name,
+      previewImage,
+      description
+    });
+    res.json(campsite);
+  } else {
+    const error = new Error("Unauthorized");
+    error.status = 403;
+    throw error;
+  }
 });
 
 module.exports = router;
