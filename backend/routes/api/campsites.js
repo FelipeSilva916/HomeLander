ac4da4496237e67ac471eb0e671ad7493b9d5b1a
Felipe Silva <felipesilva916@gmail.com>
2022-09-17 09:52:52 -0700

    create campsite working

--- backend/routes/api/campsites.js
+++ backend/routes/api/campsites.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const { Campsite } = require("../../db/models");
+const { requireAuth } = require("../../utils/auth");
+
+// ======== POST /api/campsites - Create a new campsite ========//
+router.post("/", requireAuth, async (req, res, next) => {
+  const { user } = req;
+  const { latitude, longitude, name, previewImage, description } = req.body;
+  console.log(user);
+  const newCampsite = await Campsite.create({
+    userId: user.id,
+    latitude,
+    longitude,
+    name,
+    previewImage,
+    description
+  });
+  res.json(newCampsite);
+});
 
 //========= GET /api/campsites - Get all campsites =========//
 router.get("/", async (req, res) => {
