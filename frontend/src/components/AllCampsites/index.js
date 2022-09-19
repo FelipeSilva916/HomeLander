9f0e82caa9a50264143c9ea11a112bc9005b62c1
Felipe Silva <felipesilva916@gmail.com>
2022-09-18 18:22:41 -0700

    loading all campsites

--- frontend/src/components/AllCampsites/index.js
+++ frontend/src/components/AllCampsites/index.js
@@ -7,19 +7,22 @@ import "./AllCampsites.css";
 const AllCampsites = () => {
   const dispatch = useDispatch();
   const campsites = useSelector((state) => Object.values(state.campsite));
-  console.log(campsites);
-  console.log("-----------------------------");
+
   useEffect(() => {
     dispatch(getAllCampsites());
   }, [dispatch]);
   return (
-    <div className="all-campsites">
+    <div className="all-campsites-wrapper">
       <h1>All Campsites</h1>
       <div className="all-campsites-container">
         {campsites.map((campsite) => (
           <div className="campsite-card" key={campsite.id}>
             <NavLink to={`/campsites/${campsite.id}`}>
-              {/* <img src={campsite.previewImage} alt={campsite.name} /> */}
+              <img
+                src={campsite.previewImage}
+                alt={campsite.name}
+                className="campsite-preview-image"
+              />
               <h2>{campsite.name}</h2>
             </NavLink>
           </div>
