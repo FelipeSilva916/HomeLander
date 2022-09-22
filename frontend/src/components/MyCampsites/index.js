8c997a34a6ad28a3dc752142d4451b1b2bd7bcc8
Felipe Silva <felipesilva916@gmail.com>
2022-09-21 19:34:52 -0700

    Update index.js

--- frontend/src/components/MyCampsites/index.js
+++ frontend/src/components/MyCampsites/index.js
@@ -1,4 +1,4 @@
-import { getOneCampsite, getMyCampsites } from "../../store/campsite";
+import { getAllCampsites, getMyCampsites } from "../../store/campsite";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -6,15 +6,29 @@ import { useParams } from "react-router-dom";
 const MyCampsites = () => {
   const dispatch = useDispatch();
   const campsites = useSelector((state) => state.campsite);
-  //   console.log(campsites);
+  const user = useSelector((state) => state.session.user);
+  //   console.log(campsites, user);
 
   useEffect(() => {
     dispatch(getMyCampsites());
+    dispatch(getAllCampsites());
   }, [dispatch]);
+
+  const myCampsites = Object.values(campsites).filter(
+    (campsite) => campsite.userId === user.id
+  );
 
   return (
     <div>
-      <h1>My Campsites </h1>
+      <h1>My Campsites</h1>
+      <div>
+        {Object.values(myCampsites).map((campsite) => (
+          <div key={campsite.id}>
+            <h3>{campsite.name}</h3>
+            <img src={campsite.previewImage} alt="campsite" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
