ad3c012ea6d52438fbcdce7fa13bd6513c68ad8c
Felipe Silva <felipesilva916@gmail.com>
2022-09-25 07:19:33 -0700

    custom logo crashes on reload

--- frontend/src/components/GoogleMap/CampsiteMaps.js
+++ frontend/src/components/GoogleMap/CampsiteMaps.js
@@ -19,7 +19,14 @@ const Map = ({ lat, lng }) => {
   return (
     <LoadScript googleMapsApiKey="AIzaSyDYELTx5aJJ9cwL9nzevICoo1CD5ILMaSI">
       <GoogleMap mapContainerStyle={mapStyles} zoom={14} center={center}>
-        <Marker position={center} />
+        <Marker
+          position={center}
+          // icon={{
+          //   url: "https://homelander.s3.us-west-1.amazonaws.com/mappin.svg",
+          //   scaledSize: new window.google.maps.Size(40, 50),
+          //   anchor: new window.google.maps.Point(15, 15)
+          // }}
+        />
       </GoogleMap>
     </LoadScript>
   );
