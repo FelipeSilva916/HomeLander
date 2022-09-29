db5f43c8689caa19a63d825a71838f85d24004a9
Felipe <87789520+FelipeSilva916@users.noreply.github.com>
2022-09-29 13:15:24 -0700

    css

--- frontend/src/components/ReviewDetails/index.js
+++ frontend/src/components/ReviewDetails/index.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getReviewsByCampsiteId } from "../../store/reviews";
 import EditReviewModal from "../EditReviewModal";
+import "./ReviewDetails.css";
 
 const ReviewsTable = ({ campsiteId }) => {
   const dispatch = useDispatch();
@@ -17,7 +18,9 @@ const ReviewsTable = ({ campsiteId }) => {
       <h1>Reviews</h1>
       {reviewsArray.map((review, i) => (
         <div key={i}>
-          <p>{review?.User?.username}</p>
+          <div>
+            <p>{review?.User?.username}</p>
+          </div>
           <p>{review?.rating}</p>
           <p>{review?.body}</p>
 
