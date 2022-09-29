76784a185ed2c60c727425ab06098421cf50b5b0
Felipe Silva <felipesilva916@gmail.com>
2022-09-28 17:46:05 -0700

    css

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -5,11 +5,8 @@ const Homepage = () => {
     <div className="homepage__container">
       <div className="homepage__contents">
         <h2 className="homepage-title">
-          <i
-            className="fa-duotone fa-campground"
-            style="--fa-primary-color: orangered;"
-          ></i>
-          ️ Welcome to HomeLander!
+          <i className="fa-duotone fa-campground" style={{ color: "" }}></i>️
+          Welcome to HomeLander!
         </h2>
         <p>Find your next adventure!</p>
       </div>
