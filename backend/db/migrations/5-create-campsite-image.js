6030617fdf4887006274ac91dc3acb8d2dcecbf9
Felipe Silva <felipesilva916@gmail.com>
2022-10-01 13:22:12 -0700

    TEXT

--- backend/db/migrations/5-create-campsite-image.js
+++ backend/db/migrations/5-create-campsite-image.js
@@ -19,7 +19,7 @@ module.exports = {
         onDelete: "CASCADE"
       },
       imageUrl: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT
       },
       createdAt: {
         allowNull: false,
