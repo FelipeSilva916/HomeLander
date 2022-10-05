c0cf020e67617dc14a560443a2b8cbfe4769fe8c
Felipe Silva <felipesilva916@gmail.com>
2022-10-04 20:26:28 -0700

    fixed favorite error

--- backend/db/seeders/3-Favorites.js
+++ backend/db/seeders/3-Favorites.js
@@ -4,22 +4,18 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert("Favorites", [
       {
-        id: 1,
         userId: 1,
         campsiteId: 1
       },
       {
-        id: 2,
         userId: 1,
         campsiteId: 2
       },
       {
-        id: 3,
         userId: 2,
         campsiteId: 3
       },
       {
-        id: 4,
         userId: 2,
         campsiteId: 2
       }
