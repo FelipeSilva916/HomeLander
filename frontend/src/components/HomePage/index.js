c7fbb0343b575177edd7dc1aee5996250ff71e47
Felipe Silva <87789520+FelipeSilva916@users.noreply.github.com>
2022-11-04 15:36:38 -0700

    working

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -16,8 +16,8 @@ const Homepage = () => {
   if (user) {
     return (
       <>
-        <>
-          <div className="homepage__container">
+        <div className="homepage__container">
+          <div className="homepage">
             <div className="homepage__contents">
               <div className="homepage__about"></div>
               <h2 className="homepage-title">
@@ -33,20 +33,36 @@ const Homepage = () => {
               <Searchbar />
             </div>
           </div>
-        </>
+        </div>
       </>
     );
   } else {
     return (
       <>
-        <h1>HEY</h1>
-        <h1>HEY</h1>
-        <h1>HEY</h1>
-        <h1>HEY</h1>
-        <h1>HEY</h1>
-        <h1>HEY</h1>
-        <h1>HEY</h1>
-        <h1>HEY</h1>
+        <div className="homepage__container">
+          <div className="homepage">
+            <div className="homepage__contents">
+              <div className="homepage__about"></div>
+              <h2 className="homepage-title">
+                <i
+                  className="fa-duotone fa-campground"
+                  style={{ color: "#ff4f00" }}
+                ></i>
+                Welcome to HomeLander!
+              </h2>
+              <p>Find your next adventure!</p>
+            </div>
+            <div className="homepage__description">
+              <p>
+                HomeLander is a great way to share your favorite camping spots
+                with friends and family. With HomeLander, you can create
+                campsites that anyone can access. They contain full details such
+                as descriptions, pictures, and coordinates. You can also add
+                campsites to your favorites list to quickly access them anytime.
+              </p>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
